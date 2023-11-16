3f1e2c5445a3bd3945aaa6649377b08470e88a9c
marikomands <marikomands@gmail.com>
2023-11-16 15:01:04 +0000

    Updated with Navi, Order and Summary components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Nav, Container, Card, Form, Row, Col } from "react-bootstrap";
+import Order from "./components/Order";
+import Summary from "./components/Summary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav activeKey="/home" style={{ background: "#0d6efd" }}>
+        <Nav.Item>
+          <Nav.Link href="/home" style={{ color: "#ffff" }}>
+            <h4>Pizza Builder Form </h4>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Container className="my-4">
+        <Card className="shadow-lg p-4">
+          <Form>
+            <Row>
+              <Col md={8}>
+                <Order />
+              </Col>
+              <Col md={4}>
+                <Summary></Summary>
+              </Col>
+            </Row>
+          </Form>
+        </Card>
+      </Container>
     </div>
   );
 }
